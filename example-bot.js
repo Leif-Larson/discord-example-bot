85f890982f5481636467ef6cdc8d7520ff241260
anotherkitten <anotherkitten@gmail.com>
2019-05-01 09:34:23 -0500

    Add logging

--- example-bot.js
+++ example-bot.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const auth = require('./auth.json');
+const fs = require('fs');
+const logfile = "joke_logs.txt"
 
 var jokeUser = "";
 var jokeStage = 0;
@@ -50,7 +52,30 @@ client.on('message', msg => {
 		
 		// if their punchline makes sense with the rest of the joke
 		if (msg.content.toLowerCase().includes(jokeContent.toLowerCase())) {
-			msg.reply("Oh that sure was a good one");
+			// log completed joke
+			fs.appendFile(logfile, msg.content + ";", (err) => {
+				if (err) throw err;
+			});
+			
+			fs.readFile(logfile, (err, data) => {
+				if (err) throw err;
+				console.log(data);
+				
+				var saidBefore = false;
+				var previousJokes = data.split(";");
+				
+				// for every previous good joke told
+				for (let joke of previousJokes) {
+					
+					// if it is not equal to the current joke
+					if (joke.trim().toLowerCase() != msg.content.trim().toLowerCase()) {
+						msg.reply("Oh that sure was a good one");
+					} // otherwise its old news
+					else {
+						msg.reply("I've heard that one before");
+					}
+				}
+			});
 		}
 		else {
 			msg.reply("I don't get it");
